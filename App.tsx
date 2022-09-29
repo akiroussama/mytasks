2f68a550571642ceefb53e04fcb8f95c1f25f238
Akir Oussama <akir.oussama@gmail.com>
2022-09-29 04:52:03 +0200

    install native base

--- App.tsx
+++ App.tsx
@@ -1,4 +1,21 @@
 import Main from "./src/screens/main";
+import { NativeBaseProvider } from 'native-base';
+import { extendTheme } from 'native-base';
+
 export default function App() {
-  return (<Main></Main>);
+  const theme = extendTheme({
+    components: {
+      Heading: {
+        baseStyle: (props: any) => {
+          return {
+            _light: { color: 'red.300' },
+            _dark: { color: 'blue.300' },
+          };
+        },
+      },
+    },
+  });
+  return (<NativeBaseProvider theme={theme}>
+   <Main></Main>
+   </NativeBaseProvider>);
 }
