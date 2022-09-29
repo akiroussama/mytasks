1975e94f2841badf9464b71d52b5bc2722646cca
Akir Oussama <akir.oussama@gmail.com>
2022-09-29 06:26:59 +0200

    add task item ( with style )

--- src/screens/about.tsx
+++ src/screens/about.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text, } from "react-native";
+import MyComponent from "../components/wilders";
 
 export default function About() {
-   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>About Screen</Text>
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <MyComponent></MyComponent>
     </View>
-  )
+  );
 }
   
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,7 +1,26 @@
-import MyComponent from "../components/my-component"
+import { useState } from "react";
+import TaskItem from "../components/task-item";
 export default function Main() {
-   return (
-    <MyComponent></MyComponent>
-  )
+  const [checked, setChecked] = useState(false);
+  const [subject, setSubject] = useState("task item");
+  const handleToggleCheckbox = () => {
+    console.log(arguments);
+    // setData(prevData => {
+    //   const newData = [...prevData]
+    //   const index = prevData.indexOf(item)
+    //   newData[index] = {
+    //     ...item,
+    //     done: !item.done
+    //   }
+    //   return newData
+    // })
+  };
+  return (
+    <TaskItem
+      isDone={checked}
+      subject={subject}
+      isEditing={false}
+      onToggleCheckbox={handleToggleCheckbox}
+    ></TaskItem>
+  );
 }
-  
