4b458d5bfcdec17962be09e7a7d8563cb0d9d34e
Akir Oussama <akir.oussama@gmail.com>
2022-09-29 07:44:37 +0200

    remove swiable to delete note

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -70,59 +70,28 @@ const TaskItem = (props: Props) => {
   );
 
   return (
-    <SwipableView
-      simultaneousHandlers={simultaneousHandlers}
-      onSwipeLeft={onRemove}
-      backView={
-        <Box
-          w="full"
-          h="full"
-          bg="red.500"
-          alignItems="flex-end"
-          justifyContent="center"
-          pr={4}
-        >
-          <Icon color="white" as={<Feather name="trash-2" />} size="sm" />
-        </Box>
-      }
+    <HStack
+      alignItems="center"
+      w="full"
+      px={4}
+      py={2}
+      bg={useColorModeValue("warmGray.50", "primary.900")}
     >
-      <HStack
-        alignItems="center"
-        w="full"
-        px={4}
-        py={2}
-        bg={useColorModeValue("warmGray.50", "primary.900")}
-      >
-        <Box width={30} height={30} mr={2}>
-          <Pressable onPress={onToggleCheckbox}>
-            <AnimatedCheckbox
-              highlightColor={highlightColor}
-              checkmarkColor={checkmarkColor}
-              boxOutlineColor={boxStroke}
-              checked={isDone}
-            />
-          </Pressable>
-        </Box>
-        {isEditing ? (
-          <Input
-            placeholder="Task"
-            value={subject}
-            variant="unstyled"
-            fontSize={19}
-            px={1}
-            py={0}
-            autoFocus
-            blurOnSubmit
-            onChange={handleChangeSubject}
-            onBlur={onFinishEditing}
+      <Box width={30} height={30} mr={2}>
+        <Pressable onPress={onToggleCheckbox}>
+          <AnimatedCheckbox
+            highlightColor={highlightColor}
+            checkmarkColor={checkmarkColor}
+            boxOutlineColor={boxStroke}
+            checked={isDone}
           />
-        ) : (
-          <Pressable onPress={onPressLabel}>
-            <Text>{subject}</Text>
-          </Pressable>
-        )}
-      </HStack>
-    </SwipableView>
+        </Pressable>
+      </Box>
+
+      <Pressable onPress={onPressLabel}>
+        <Text>{subject}</Text>
+      </Pressable>
+    </HStack>
   );
 };
 
