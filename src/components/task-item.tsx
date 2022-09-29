92bf8c3b31e16783704350ea8600c264fae38223
Akir Oussama <akir.oussama@gmail.com>
2022-09-29 08:12:31 +0200

    forward 10 remove onPressLabel and scrolll view ref

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,27 +1,15 @@
-import React, { useCallback } from "react";
-import { PanGestureHandlerProps } from "react-native-gesture-handler";
-import { NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
-import {
-  Text,
-  Pressable,
-  Box,
-  HStack,
-  useColorModeValue,
-  Icon,
-  Input,
-  useToken,
-} from "native-base";
+import React from "react";
+import { Text, Pressable, Box, HStack } from "native-base";
 import AnimatedCheckbox from "react-native-checkbox-reanimated";
 
 interface Props {
   isDone: boolean;
   onToggleCheckbox?: () => void;
-  onPressLabel?: () => void;
   subject: string;
 }
 
 const TaskItem = (props: Props) => {
-  const { isDone, onToggleCheckbox, subject, onPressLabel } = props;
+  const { isDone, onToggleCheckbox, subject } = props;
 
   return (
     <HStack alignItems="center" w="full">
@@ -35,10 +23,7 @@ const TaskItem = (props: Props) => {
           />
         </Pressable>
       </Box>
-
-      <Pressable onPress={onPressLabel}>
-        <Text>{subject}</Text>
-      </Pressable>
+      <Text>{subject}</Text>
     </HStack>
   );
 };
--- src/components/task-list.tsx
+++ src/components/task-list.tsx
@@ -11,65 +11,38 @@ interface TaskItemData {
 interface TaskListProps {
   data: Array<TaskItemData>;
   onToggleItem: (item: TaskItemData) => void;
-  onPressLabel: (item: TaskItemData) => void;
 }
 
 interface TaskItemProps {
   data: TaskItemData;
   onToggleItem: (item: TaskItemData) => void;
-  onPressLabel: (item: TaskItemData) => void;
 }
 
 export const AnimatedTaskItem = (props: TaskItemProps) => {
-  const { data, onToggleItem, onPressLabel } = props;
+  const { data, onToggleItem } = props;
   const handleToggleCheckbox = useCallback(() => {
     onToggleItem(data);
   }, [data, onToggleItem]);
-  const handlePressLabel = useCallback(() => {
-    onPressLabel(data);
-  }, [data, onPressLabel]);
   return (
-    <View
-      from={{
-        opacity: 0,
-        scale: 0.5,
-        marginBottom: -46,
-      }}
-      animate={{
-        opacity: 1,
-        scale: 1,
-        marginBottom: 0,
-      }}
-      exit={{
-        opacity: 0,
-        scale: 0.5,
-        marginBottom: -46,
-      }}
-    >
-      <TaskItem
-        subject={data.subject}
-        isDone={data.done}
-        onToggleCheckbox={handleToggleCheckbox}
-        onPressLabel={handlePressLabel}
-      />
-    </View>
+    <TaskItem
+      subject={data.subject}
+      isDone={data.done}
+      onToggleCheckbox={handleToggleCheckbox}
+    />
   );
 };
 
 export default function TaskList(props: TaskListProps) {
-  const { data, onToggleItem, onPressLabel } = props;
-  const refScrollView = useRef(null);
+  const { data, onToggleItem } = props;
 
   return (
-    <ScrollView ref={refScrollView}>
+    <ScrollView>
       <AnimatePresence>
         {data.map(item => (
           <AnimatedTaskItem
             key={item.id}
             data={item}
-            simultaneousHandlers={refScrollView}
             onToggleItem={onToggleItem}
-            onPressLabel={onPressLabel}
           />
         ))}
       </AnimatePresence>
