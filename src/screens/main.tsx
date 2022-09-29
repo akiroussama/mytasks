ec33b6901108132d705c4f90470ae9a147585bdb
Akir Oussama <akir.oussama@gmail.com>
2022-09-29 07:24:15 +0200

    add tasklist full features

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,47 +1,115 @@
-import { useCallback, useState } from "react";
+import React, { useCallback, useState } from "react";
+import { Icon, VStack, useColorModeValue, Fab } from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import AnimatedColorBox from "../components/animated-color-box";
 import TaskList from "../components/task-list";
+import shortid from "shortid";
+import Masthead from "../components/masthead";
+import NavBar from "../components/navbar";
+
 const initialData = [
   {
-    id: "1",
+    id: shortid.generate(),
     subject: "Buy movie tickets for Friday",
     done: false,
   },
   {
-    id: "2",
+    id: shortid.generate(),
     subject: "Make a React Native tutorial",
     done: false,
   },
 ];
-export default function Main() {
-  const [checked, setChecked] = useState(false);
-  const [subject, setSubject] = useState("task item");
-  const handleToggleCheckbox = () => {
-    console.log(arguments);
-    // setData(prevData => {
-    //   const newData = [...prevData]
-    //   const index = prevData.indexOf(item)
-    //   newData[index] = {
-    //     ...item,
-    //     done: !item.done
-    //   }
-    //   return newData
-    // })
-  };
-  const handleToggleTaskItem = () => {};
-  const handleChangeTaskItemSubject = () => {};
-  const handleFinishEditingTaskItem = () => {};
-  const handlePressTaskItemLabel = () => {};
-  const handleRemoveItem = () => {};
-  const editingItemId = "test";
+
+export default function MainScreen() {
+  const [data, setData] = useState(initialData);
+  const [editingItemId, setEditingItemId] = useState<string | null>(null);
+
+  const handleToggleTaskItem = useCallback(item => {
+    setData(prevData => {
+      const newData = [...prevData];
+      const index = prevData.indexOf(item);
+      newData[index] = {
+        ...item,
+        done: !item.done,
+      };
+      return newData;
+    });
+  }, []);
+  const handleChangeTaskItemSubject = useCallback((item, newSubject) => {
+    setData(prevData => {
+      const newData = [...prevData];
+      const index = prevData.indexOf(item);
+      newData[index] = {
+        ...item,
+        subject: newSubject,
+      };
+      return newData;
+    });
+  }, []);
+  const handleFinishEditingTaskItem = useCallback(_item => {
+    setEditingItemId(null);
+  }, []);
+  const handlePressTaskItemLabel = useCallback(item => {
+    setEditingItemId(item.id);
+  }, []);
+  const handleRemoveItem = useCallback(item => {
+    setData(prevData => {
+      const newData = prevData.filter(i => i !== item);
+      return newData;
+    });
+  }, []);
+
   return (
-    <TaskList
-      data={initialData}
-      onToggleItem={handleToggleTaskItem}
-      onChangeSubject={handleChangeTaskItemSubject}
-      onFinishEditing={handleFinishEditingTaskItem}
-      onPressLabel={handlePressTaskItemLabel}
-      onRemoveItem={handleRemoveItem}
-      editingItemId={editingItemId}
-    />
+    <AnimatedColorBox
+      flex={1}
+      bg={useColorModeValue("warmGray.50", "primary.900")}
+      w="full"
+    >
+      <Masthead
+        title="What's up, Takuya!"
+        image={require("../assets/masthead.png")}
+      >
+        <NavBar />
+      </Masthead>
+      <VStack
+        flex={1}
+        space={1}
+        bg={useColorModeValue("warmGray.50", "primary.900")}
+        mt="-20px"
+        borderTopLeftRadius="20px"
+        borderTopRightRadius="20px"
+        pt="20px"
+      >
+        <TaskList
+          data={data}
+          onToggleItem={handleToggleTaskItem}
+          onChangeSubject={handleChangeTaskItemSubject}
+          onFinishEditing={handleFinishEditingTaskItem}
+          onPressLabel={handlePressTaskItemLabel}
+          onRemoveItem={handleRemoveItem}
+          editingItemId={editingItemId}
+        />
+      </VStack>
+      <Fab
+        position="absolute"
+        renderInPortal={false}
+        size="sm"
+        icon={<Icon color="white" as={<AntDesign name="plus" />} size="sm" />}
+        colorScheme={useColorModeValue("blue", "darkBlue")}
+        bg={useColorModeValue("blue.500", "blue.400")}
+        onPress={() => {
+          const id = shortid.generate();
+          setData([
+            {
+              id,
+              subject: "",
+              done: false,
+            },
+            ...data,
+          ]);
+          setEditingItemId(id);
+        }}
+      />
+    </AnimatedColorBox>
   );
 }
